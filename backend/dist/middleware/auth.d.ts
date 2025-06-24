439d5ca007a1ee8c7ae61e228e81932f7bf888a3
Chris Poupart <chris.poupart@gmail.com>
2025-06-24 13:28:06 -0400

    feat: 🔥 remove compiled backend files

--- backend/dist/middleware/auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { User } from '../types';
-interface AuthenticatedRequest extends Request {
-    user?: User;
-}
-export declare const authenticateToken: (req: AuthenticatedRequest, res: Response, next: NextFunction) => void;
-export declare const optionalAuth: (req: AuthenticatedRequest, res: Response, next: NextFunction) => void;
-export {};
-//# sourceMappingURL=auth.d.ts.map
